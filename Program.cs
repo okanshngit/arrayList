252e3c6ba269d0a836a56701bd4c9796f26d9622
Okan Sahin <okan.shn@hotmail.com>
2022-02-03 13:35:27 +0300

    .

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace arrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sytem.Collctions
+
+                ArrayList liste = new ArrayList();
+                liste.Add(8);
+                liste.Add("Okan");
+                liste.Add(true);
+                liste.Add('A');
+
+
+                Console.WriteLine(liste[1]);
+
+
+                //AddRange birden fazla elemanı toplu halde ekleme.
+
+                string[] renkler = {"Kırmızı","Sarı"};
+                liste.AddRange(renkler);
+
+                List<int> sayilar = new List<int>(){1,5,6,78};
+
+                liste.AddRange(sayilar);
+
+                //Sorting
+                    sayilar.Sort();
+
+                //Reverse
+                    liste.Reverse();
+
+                // Clear
+                    liste.Clear();
+
+                
+
+
+
+                
+
+
+
+
+
+
+        }
+    }
+}
